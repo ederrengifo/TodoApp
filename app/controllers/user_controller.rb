d2b25e0ed6de7331c197ea282dc4d4e8aa5da7ac
Eder Rengifo <ederrengifo@Air-de-Eder-3.lan>
2016-10-11 16:47:29 -0500

    add favicon and static pages for auth

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,13 @@
+class UserController < ApplicationController
+  def login
+  end
+
+  def signup
+  end
+
+  def forgot_password
+  end
+
+  def new_password
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'user/login'
+
+  get 'user/signup'
+
+  get 'user/forgot_password'
+
+  get 'user/new_password'
+
   resources :tools
   resources :notes do
     resources :notes_lists do
@@ -12,4 +20,5 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "todo_app_lists#index"
+
 end
