10363350845fc34781a13fb4a9ce68c2f764a766
Eder Rengifo <ederrengifo@MacBook-Air-de-Eder-3.local>
2017-01-09 00:03:22 -0500

    removing migration problem

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 20170109012007) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.integer  "board_id"
-    t.boolean  "favorite",   default: false
   end
 
   create_table "notes", force: :cascade do |t|
