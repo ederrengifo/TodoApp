822302d53446c16dd1f7f204f48e359ccd206177
Eder Rengifo <ederrengifo@MacBook-Air-de-Eder-3.local>
2017-01-17 20:59:19 -0500

    adding mail styles

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,10 +1,10 @@
 class UserNotifierMailer < ApplicationMailer
-  default :from => 'hello@todoable.co'
+  default :from => 'doable@todoable.co'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
     @user = user
     mail( :to => @user.email,
-    :subject => 'Thanks for joining to Doable!' )
+    :subject => 'Thanks and welcome aboard!' )
   end
 end
